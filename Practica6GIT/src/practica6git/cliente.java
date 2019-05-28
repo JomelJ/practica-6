c1d15dc1c4b4808981a9b2a71b1308daeacff250
dam20 <dam20@PC20.aula1.com>
2019-05-28 08:57:41 +0200

    Segundo Commit Javier:Se crea la clase cliente y escribo el codigo

---
+++ Practica6GIT/src/practica6git/cliente.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica6git;
+
+/**
+ *
+ * @author dam20
+ */
+public class cliente {
+    private int id;
+    private String nombre;
+    private String apellidos;
+    private int edad;
+    private String direccion;
+    private String telefono;
+
+    public cliente() {
+    }
+
+    public cliente(int id, String nombre, String apellidos, int edad, String direccion, String telefono) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.edad = edad;
+        this.direccion = direccion;
+        this.telefono = telefono;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+}
