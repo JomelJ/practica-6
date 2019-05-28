acc5589001f8c74433430da0b90145cb3f7168e5
dam21 <dam21@PC21.aula1.com>
2019-05-28 09:19:43 +0200

    Hecho el código de la clase cuentaBancaria.

--- Practica6GIT/src/practica6git/cuentaBancaria.java
+++ Practica6GIT/src/practica6git/cuentaBancaria.java
@@ -10,5 +10,50 @@ package practica6git;
  * @author Alejandro
  */
 public class cuentaBancaria {
+    private String nombre;
+    private int saldo;
+    private int id;
+    private String cliente;
+
+    public cuentaBancaria(String nombre, int saldo, int id, String cliente) {
+        this.nombre = nombre;
+        this.saldo = saldo;
+        this.id = id;
+        this.cliente = cliente;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(int saldo) {
+        this.saldo = saldo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(String cliente) {
+        this.cliente = cliente;
+    }
+    
+    
     
 }
